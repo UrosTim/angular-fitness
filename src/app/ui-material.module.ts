f7cbde76089c6aeb9eed3fb27e2ad0f0fd7b0489
UrosTim <urostimapps@gmail.com>
2024-06-10 16:36:07 +0200

    sidenav and toolbar start, signup form

--- src/app/ui-material.module.ts
+++ src/app/ui-material.module.ts
@@ -15,6 +15,18 @@
 :: Input ::
     https://material.angular.io/components/input/overview
 
+:: Datepicker ::
+    https://material.angular.io/components/datepicker/overview
+
+:: Checkbox :: 
+    https://material.angular.io/components/checkbox/overview
+
+:: Sidenav ::
+    https://material.angular.io/components/sidenav/overview
+
+:: Toolbar ::
+    https://material.angular.io/components/toolbar/overview
+
 */
 
 import { NgModule } from "@angular/core";
@@ -22,10 +34,35 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+    imports: [
+        MatButtonModule, 
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule, 
+        MatNativeDateModule, 
+        MatCheckboxModule, 
+        MatSidenavModule,
+        MatToolbarModule
+    ],
+    exports: [
+        MatButtonModule, 
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule, 
+        MatNativeDateModule, 
+        MatCheckboxModule, 
+        MatSidenavModule,
+        MatToolbarModule
+    ]
 })
 
 export class MaterialModule {}
